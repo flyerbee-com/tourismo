a89d84c595199a854b68a20525bf883f73e8f626
Nicolas Baer <baer@silberruecken.ch>
2017-10-28 03:13:23 +0200

    almost ready for presentation :)

--- App.js
+++ App.js
@@ -9,7 +9,8 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
 
 import {
@@ -19,14 +20,14 @@ import {
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
+  'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  'Shake or press menu button for dev menu',
 });
 
 export default class App extends Component {
 
-  constructor(){
+  constructor() {
     super()
     this.state = {
       closest_beacon: null
@@ -35,6 +36,12 @@ export default class App extends Component {
 
   componentWillMount() {
     let detected_supported_beacons = new Map();
+    let supported_beacons = new Set();
+
+    fetch("http://directus.phy.one/api/1.1/tables/beacons/rows?access_token=xI2ZIGDw8rU7dgB59V15CQDmlPW5HXeT")
+      .catch(err => console.log("error fetching " + err))
+      .then(res => { Array.from(JSON.parse(res._bodyInit).data).forEach(b => supported_beacons.add(b.beacon_name)) })
+
     this.manager = new BleManager({
       restoreStateIdentifier: 'testBleBackgroundMode',
       restoreStateFunction: bleRestoreState => {
@@ -43,34 +50,37 @@ export default class App extends Component {
     this.manager.onStateChange((newState) => {
 
       if (newState === 'PoweredOn') {
-        const supported_beacons = new Set([
-          'abeacon_B8B1',
-          'abeacon_B8B0',
-          'abeacon_B8B2',
-          'abeacon_B8FB',
-        ]);
-
         this.manager.startDeviceScan(null, null, (error, device) => {
-          if(error) return
-          if(supported_beacons.has(device.name)) detected_supported_beacons.set(device.name, device)
+          if (error) return
+          if (supported_beacons.has(device.name) && device != null && device.name != null) {
+            detected_supported_beacons.set(device.name, device)
+            console.log("received signal from " + device.name)
+          }
         });
 
         setInterval(() => {
+          detected_supported_beacons.forEach((value, key) => {
+            console.log(key, value.rssi);
+          });
+          console.log("===========")
           const sorted = Array.from(detected_supported_beacons.values()).sort((a, b) => a.rssi < b.rssi)
           const closest = sorted[0]
 
           fetch("http://directus.phy.one/api/1.1/tables/beacons/rows?access_token=xI2ZIGDw8rU7dgB59V15CQDmlPW5HXeT")
-          .catch(err => console.log("error fetching " + err))
-          .then(res => {
-            const contents = JSON.parse(res._bodyInit).data
-            const theBeacon = Array.from(contents).filter(b => b.beacon_name == closest.name)[0]
-            this.setState({closest_beacon: theBeacon.title})
-          })
+            .catch(err => console.log("error fetching " + err))
+            .then(res => {
+              if (closest && closest.name) {
+                const contents = JSON.parse(res._bodyInit).data
+                const filtered = Array.from(contents).filter(b => b.beacon_name == closest.name)
+                const theBeacon = filtered[0]
+                this.setState({ closest_beacon: theBeacon })
+              }
+            })
 
-          this.setState({closest_beacon: closest.name})
+
         }, 3000);
       }
-    });    
+    });
   }
 
   componentWillUnmount() {
@@ -79,16 +89,44 @@ export default class App extends Component {
   }
 
   render() {
-    let {closest_beacon} = this.state;
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          {closest_beacon}
+    let { closest_beacon } = this.state;
+    if (closest_beacon != null) {
+      let img = null
+      if (closest_beacon.image != null) img = (<Image
+        style={{
+          alignSelf: 'center',
+          height: 150,
+          width: 150,
+          borderWidth: 1,
+          borderRadius: 75
+        }}
+        source={{ uri: 'http://directus.phy.one' + closest_beacon.image.data.url }} resizeMode="contain"
+      />)
+
+      return (
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            {closest_beacon.title}
+          </Text>
+          {img}
+          <Text>
+            {closest_beacon.description}
+          </Text>
+          <Text>
+            {closest_beacon.beacon}
+          </Text>
+
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            loading the unexpected ...
         </Text>
-        { closest_beacon && <Text>is the closest beacon</Text> }
-        { closest_beacon === null && <Text>loading the unexpected ...</Text> }
-      </View>
-    );
+        </View>
+      )
+    }
   }
 }
 
